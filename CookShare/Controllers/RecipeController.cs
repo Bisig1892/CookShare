b9f2384bb1e49cad83f9c24eb1cc39a662044f1a
Tyler Bisig <56323248+Bisig1892@users.noreply.github.com>
2020-11-09 21:55:44 -0800

    Issue#4 create recipe page (#19)
    * Created view that shows list of recipes
    
    * Created Add page
    
    * Added recipe tab in nav
    
    * changed anchor tags for edit delete and add recipe to buttons

---
+++ CookShare/Controllers/RecipeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CookShare.Data;
+using CookShare.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookShare.Controllers
+{
+    public class RecipeController : Controller
+    {
+
+        public readonly RecipeContext _context;
+
+        public RecipeController(RecipeContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Displays a view that lists all recipes
+        /// </summary>
+        public IActionResult Index()
+        {
+            // Get all recipes from database
+            List<Recipe> recipes = _context.Recipes.ToList();
+            // Send list of recipes to view to be displayed
+            return View(recipes);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Recipe r)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Recipes.Add(r);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
