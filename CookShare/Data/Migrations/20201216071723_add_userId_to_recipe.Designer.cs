e8960d8a82026860d5791ec82f556035b15a4d63
Tyler <bisig1892@gmail.com>
2020-12-15 23:47:35 -0800

    userId was added to the recipe model

--- CookShare/Data/Migrations/20201216071723_add_userId_to_recipe.Designer.cs
+++ CookShare/Data/Migrations/20201216071723_add_userId_to_recipe.Designer.cs
@@ -4,14 +4,16 @@ using CookShare.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CookShare.Data.Migrations
 {
     [DbContext(typeof(RecipeContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201216071723_add_userId_to_recipe")]
+    partial class add_userId_to_recipe
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CookShare/Data/Migrations/20201216071723_add_userId_to_recipe.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CookShare.Data.Migrations
+{
+    public partial class add_userId_to_recipe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "userId",
+                table: "Recipes",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "userId",
+                table: "Recipes");
+        }
+    }
+}
